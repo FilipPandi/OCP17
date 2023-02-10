cfb5b7a0207b22b7f8b0ca6242b03dc723d7650c
filip <filip.pandi@inovatrend.com>
2023-02-10 14:03:16 +0100

    Add operators.

---
+++ src/main/java/ocp/exame/OperatorsExercise.java
@@ -0,0 +1,14 @@
+package ocp.exame;
+
+public class OperatorsExercise {
+
+    public static void main(String[] args) {
+
+        byte apples = 5;
+        short oranges = 10;
+
+        int ban = apples + oranges;
+        float ban2 = apples + oranges;
+        float ban3 = apples + oranges;
+    }
+}
