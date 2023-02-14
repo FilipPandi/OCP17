283940c420911933bf47ff34342ac38efbf32564
filip <filip.pandi@inovatrend.com>
2023-02-14 15:04:56 +0100

    Add statements.

--- src/main/java/ocp/exame/DecisionStatementsExercise.java
+++ src/main/java/ocp/exame/DecisionStatementsExercise.java
@@ -18,8 +18,32 @@ public class DecisionStatementsExercise {
             System.out.println(data);
         }
 
-        if (!(x instanceof Integer x)) {
+        final Integer counter = 5;
 
+        //Normal switch with breaks (switch without breaks get into all cases).
+        switch (counter) {
+            case 1:
+                System.out.println("it is one!");
+                break;
+            case 2:
+                System.out.println("it is two!");
+                break;
+            case 3:
+                System.out.println("it is three!");
+                break;
+            default:
+                System.out.println("I'm default!");
+                break;
         }
+
+        //Switch with lambdas (lambda works like break).
+        switch (counter) {
+            case 1 -> System.out.println("it is one!");
+            case 2 -> System.out.println("it is two!");
+            case 3 -> System.out.println("it is three!");
+            default -> System.out.println("I'm default!");
+        }
+
+
     }
 }
